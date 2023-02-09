9eaea53bf5ac9c757411118cdce32f5c1717b997
Puncoz Nepal <puncoz138@gmail.com>
2023-02-09 12:58:43 +0900

    service discovery

--- order-service/src/main/java/com/microcommerce/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microcommerce/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
 
     public void placeOrder(OrderRequest orderRequest) {
@@ -34,8 +34,9 @@ public class OrderService {
         List<String> skuCodes = order.getOrderItemList().stream().map(OrderItem::getSkuCode).toList();
 
         // call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventories", uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes)
+        InventoryResponse[] inventoryResponses = webClientBuilder.build()
+                .get()
+                .uri("http://inventory-service/api/inventories", uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes)
                         .build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
